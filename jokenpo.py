1244a4037b5f618cfa8efde05763ecbba19449aa
Leandro Cardoso <leandrolimacardoso@hotmail.com>
2022-07-02 12:29:22 -0300

    all choices

--- jokenpo.py
+++ jokenpo.py
@@ -1,9 +1,13 @@
+if __name__ == '__main__':
+    from random import randint
+
 class Jokenpo:
     def __init__(self):
         self.gamemode = 0
         self.players = ['', '']
         self.scores = [0, 0]
         self.choices = ['', '']
+        self.all_choices = ['rock', 'paper', 'scissors']
     def set_game_mode(self, gamemode = 0):
         '''Gamemode = 0 (P1 VS CPU), Gamemode = 1 (P1 VS P2)'''
         gamemodes = [0, 1]
@@ -25,24 +29,33 @@ class Jokenpo:
             if player == self.players[i]:
                 self.scores[i] += score
     def make_choices(self, choice1, choice2):
+        if self.gamemode == 0:
+            choice2 = self.all_choices[randint(0, 2)]
         self.choices[0] = choice1
         self.choices[1] = choice2
+        # VERIFICAR SE OPÇÃO EXISTE
+    # FUNÇÃO PARA VERIFICAR SE P1 GANHOU, EMPATOU OU PERDEU
+    def winner(self) -> str:
+        '''Return the winner (P1 name, P2 name or None)'''
+        pass
 
-# Tests:
-game = Jokenpo()
+if __name__ == '__main__':
+    # Tests:
+    game = Jokenpo()
 
-game.set_players(' leandro   cardoso', ' luna  dark')
-print('Gamemode:', game.gamemode, 'Players:', game.players)
-game.set_game_mode(1)
-game.set_players(' leandro   cardoso', ' luna  dark')
-print('Gamemode:', game.gamemode, 'Players:', game.players)
+    game.set_players(' leandro   cardoso', ' luna  dark')
+    print('Gamemode:', game.gamemode, 'Players:', game.players)
+    game.set_game_mode(1)
+    game.set_players(' leandro   cardoso', ' luna  dark')
+    print('Gamemode:', game.gamemode, 'Players:', game.players)
 
-game.win_score(game.players[0])
-game.win_score(game.players[0])
-game.win_score(game.players[1])
-print(game.players[0] + ':', game.scores[0])
-print(game.players[1] + ':', game.scores[1])
+    game.win_score(game.players[0])
+    game.win_score(game.players[0])
+    game.win_score(game.players[1])
+    print(game.players[0] + ':', game.scores[0])
+    print(game.players[1] + ':', game.scores[1])
 
-game.make_choices('scissors', 'paper')
-print(game.players[0] + ':', game.choices[0])
-print(game.players[1] + ':', game.choices[1])
+    game.set_game_mode(0)
+    game.make_choices('scissors', 'paper')
+    print(game.players[0] + ':', game.choices[0])
+    print(game.players[1] + ':', game.choices[1])
