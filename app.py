d6032aea7c9e114cbd472e63c323a4907af37e92
Leandro Cardoso <leandrolimacardoso@hotmail.com>
2022-07-01 21:11:36 -0300

    Restructuring

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 '''
-1 - Criar imagens para serem usadas
 2 - Logica basica
 3 - Player vs player
 4 - Quadro de pontos
@@ -11,7 +10,7 @@ class Jokenpo:
     player2 = ''
     player1Score = 0
     player2Score = 0
-    player1Choice = ''
+    player1Choice = '' # rock, scissors and paper
     player2Choice = ''
     turn = ''
     def setGameMode(self):
@@ -41,3 +40,39 @@ class Jokenpo:
 # Tests
 print(Jokenpo.player1Score, Jokenpo.player1Choice)
 Jokenpo.start(Jokenpo)
+
+# Interface
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QPushButton, QLabel
+from PyQt5.QtGui import QPixmap
+
+app = QApplication(sys.argv)
+window = QWidget()
+window.setWindowTitle('Jokenpo')
+window.setFixedWidth(800)
+window.setFixedHeight(600)
+window.setStyleSheet('background: #000000')
+
+imageScissors = QPixmap('images/scissors.png')
+imageScissorsLabel = QLabel()
+imageScissorsLabel.setPixmap(imageScissors)
+imageScissorsLabel.setStyleSheet('')
+
+imagePaper = QPixmap('images/paper.png')
+imagePaperLabel = QLabel()
+imagePaperLabel.setPixmap(imagePaper)
+imagePaperLabel.setStyleSheet('')
+
+button = QPushButton('Play')
+button.setStyleSheet(
+    '*{border: 4px solid "#009933"; border-radius: 15px; font-size: 35px; color: "white"; padding: 25px 0px; margin: 10px 20px}' +
+    '*:hover{border-color: "#CCFFCC"; background: "#009933"}')
+
+grid = QGridLayout()
+grid.addWidget(imageScissorsLabel, 0, 0)
+grid.addWidget(imagePaperLabel, 0, 1)
+grid.addWidget(button, 1, 0)
+window.setLayout(grid)
+
+window.show()
+sys.exit(app.exec())
---
+++ jokenpo.py
@@ -0,0 +1,48 @@
+class Jokenpo:
+    def __init__(self):
+        self.gamemode = 0
+        self.players = ['', '']
+        self.scores = [0, 0]
+        self.choices = ['', '']
+    def set_game_mode(self, gamemode = 0):
+        '''Gamemode = 0 (P1 VS CPU), Gamemode = 1 (P1 VS P2)'''
+        gamemodes = [0, 1]
+        if self.gamemode not in gamemodes:
+            self.gamemode = 0
+        else:
+            self.gamemode = gamemode
+    def set_players(self, p1 = 'p1', p2 = 'p2'):
+        p1 = str(p1).title().split()
+        p1 = ' '.join(p1)
+        if self.gamemode == 0:
+            p2 = 'CPU'
+        elif self.gamemode == 1:
+            p2 = str(p2).title().split()
+            p2 = ' '.join(p2)
+        self.players = [p1, p2]
+    def win_score(self, player, score = 1):
+        for i in range(len(self.players)):
+            if player == self.players[i]:
+                self.scores[i] += score
+    def make_choices(self, choice1, choice2):
+        self.choices[0] = choice1
+        self.choices[1] = choice2
+
+# Tests:
+game = Jokenpo()
+
+game.set_players(' leandro   cardoso', ' luna  dark')
+print('Gamemode:', game.gamemode, 'Players:', game.players)
+game.set_game_mode(1)
+game.set_players(' leandro   cardoso', ' luna  dark')
+print('Gamemode:', game.gamemode, 'Players:', game.players)
+
+game.win_score(game.players[0])
+game.win_score(game.players[0])
+game.win_score(game.players[1])
+print(game.players[0] + ':', game.scores[0])
+print(game.players[1] + ':', game.scores[1])
+
+game.make_choices('scissors', 'paper')
+print(game.players[0] + ':', game.choices[0])
+print(game.players[1] + ':', game.choices[1])
